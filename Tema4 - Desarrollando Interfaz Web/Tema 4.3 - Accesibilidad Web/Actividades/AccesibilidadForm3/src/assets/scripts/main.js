46baa12e6613b74ce6cad14f04da4d919d1f890a
unknown <pablomp1301@gmail.com>
2024-03-05 14:37:36 +0100

    PabloMP - Subida Accesibilidad Form 3 Cambios - 05/03/24

--- Tema4 - Desarrollando Interfaz Web/Tema 4.3 - Accesibilidad Web/Actividades/AccesibilidadForm3/src/assets/scripts/main.js
+++ Tema4 - Desarrollando Interfaz Web/Tema 4.3 - Accesibilidad Web/Actividades/AccesibilidadForm3/src/assets/scripts/main.js
@@ -16,6 +16,27 @@ function onIniciar() {
       bloquesTexto[i].classList.add("activo");
     });
   });
+
+  // CÓDIGO DEL ACORDEON 
+  const h2Titulo = document.querySelectorAll(".divContenedor__titulo");
+  const pContenido = document.querySelectorAll(".divContenedor__contenido");
+
+  console.log(h2Titulo);
+
+  h2Titulo.forEach((nodo, i)=> {
+
+    console.log("Nodo: ", nodo);
+
+    nodo.addEventListener("click",  ()=> {
+      
+      if (pContenido[i].classList.contains("divContenedor__contenido--activo") ) {
+        pContenido[i].classList.remove('divContenedor__contenido--activo'); 
+      } else {
+        pContenido[i].classList.add('divContenedor__contenido--activo');
+      }
+    })
+  })
+
 }
 
 window.onload = onIniciar();
